0a2265166d0c83d141ca6a9ea92c355c71fc9760
Dan Bjorge <danielbj@microsoft.com>
2022-09-22 17:25:31 -0400

    Implement playNote functionality

--- src/music.ts
+++ src/music.ts
@@ -3,7 +3,8 @@ enum Music {
     Cat = 1,
 }
 
-//% color="#ff0000" weight=27 icon="\u266b"
+//% color=#E63022 weight=40 icon="\uf025"
+//% groups='["Music Discs", "Sound", "Notes", "Volume"]'
 namespace music {
     //% block="play game music $music"
     export function playMusic(music: Music): void {
--- src/sound.ts
+++ src/sound.ts
@@ -6,14 +6,20 @@ enum Sound {
     Chicken = 3
 }
 
-//% color="#ff0000" weight=27 icon="\u266a"
-namespace sound {
-    //% block="play sound $sound || at volume $volume|at pitch $pitch"
-    //% sound.defl=Sound.PianoNote
-    //% volume.defl=10 volume.min=0 volume.max=10
+namespace music {
+    /**
+     * Plays a Minecraft game sound.
+     * @param sound pitch of the tone to play in Hertz (Hz), eg: Note.C
+     * @param pitch tone duration in milliseconds (ms)
+     */
+    //% help=sound/play-sound weight=90
+    //% blockId=sound_play_sound block="play|sound $sound|at pitch $pitch" blockGap=8
+    //% parts="headphone"
+    //% useEnumVal=1
+    //% group="Sound"
     //% pitch.defl=0.7 pitch.min=0 pitch.max=2
-    export function playSound(sound: Sound, volume: number, pitch: number): void {
-        player.execute(`playsound ${minecraftSoundId(sound)} @s ~ ~ ~ ${volume} ${pitch}`)
+    export function playSound(sound: Sound, pitch: number): void {
+        player.execute(`playsound ${minecraftSoundId(sound)} @a ~ ~ ~ ${music.volumeInGameUnits} ${pitch}`)
     }
 
     function minecraftSoundId(sound: Sound) {
