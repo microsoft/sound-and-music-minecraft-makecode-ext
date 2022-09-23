7ab855594e4580e1d00b5cef777d323224b39d7c
Kenny Duenke <kduenke@gmail.com>
2022-09-23 16:39:33 -0500

    Add music blocks (#2)
    - Add music blocks
    - Change volume slider to 0...100
    
    Co-authored-by: Kenny Duenke <keduenke@microsoft.com>

--- src/music.ts
+++ src/music.ts
@@ -1,20 +1,93 @@
-enum Music {
-    //% block="Cat"
-    Cat = 1,
+declare const enum MusicDisc {
+    //% block="C418 - 11"
+    Eleven,
+    //% block="C418 - 13"
+    Thirteen,
+    //% block="C418 - blocks"
+    Blocks,
+    //% block="C418 - cat"
+    Cat,
+    //% block="C418 - chirp"
+    Chirp,
+    //% block="C418 - far"
+    Far,
+    //% block="C418 - mall"
+    Mall,
+    //% block="C418 - mellohi"
+    Mellohi,
+    //% block="C418 - stal"
+    Stal,
+    //% block="C418 - strad"
+    Strad,
+    //% block="C418 - wait"
+    Wait,
+    //% block="C418 - ward"
+    Ward,
+    //% block="Lena Raine - otherside"
+    Otherside,
+    //% block="Lena Raine - Pigstep"
+    Pigstep
+}
+
+declare const enum MusicOption {
+    //% block="once"
+    Once,
+    //% block="forever"
+    Forever
 }
 
 //% color=#E63022 weight=40 icon="\uf025"
 //% groups='["Music Discs", "Sound", "Notes", "Volume"]'
 namespace music {
-    //% block="play game music $music"
-    export function playMusic(music: Music): void {
-        player.execute(`music play ${minecraftMusicId(music)}`);
+    /**
+     * Play a Minecraft music disc.
+     * @param builtInMusicDisc a built-in Minecraft music disc you wish to play
+     * @param musicOption the play option for the music disc
+     */
+    //% block="play music $builtInMusicDisc repeating $musicOption"
+    export function playMusic(builtInMusicDisc: MusicDisc, musicOption: MusicOption = MusicOption.Once): void {
+        let command = "";
+
+        switch (musicOption) {
+            case MusicOption.Once:
+                command = `music play ${minecraftTrackName(builtInMusicDisc)} ${music.volumeInGameUnits}`;
+                break;
+
+            case MusicOption.Forever:
+                command = `music play ${minecraftTrackName(builtInMusicDisc)} ${music.volumeInGameUnits} 0 loop`;
+                break;
+
+            default: throw `Unrecognized MusicOption enum value: ${musicOption}`;
+        }
+
+        player.execute(command);
     }
 
-    function minecraftMusicId(music: Music) {
-        switch (music) {
-            case Music.Cat: return "cat";
-            default: throw `Unrecognized music enum value ${music}`;
+    /**
+     * Stop the music.
+     */
+    //% block="stop music"
+    export function stopMusic(): void {
+        player.execute("music stop");
+    }
+
+    function minecraftTrackName(musicDisc: MusicDisc) {
+        switch (musicDisc) {
+            case MusicDisc.Eleven: return "record.11";
+            case MusicDisc.Thirteen: return "record.13";
+            case MusicDisc.Blocks: return "record.blocks";
+            case MusicDisc.Cat: return "record.cat";
+            case MusicDisc.Chirp: return "record.chirp";
+            case MusicDisc.Far: return "record.far";
+            case MusicDisc.Mall: return "record.mall";
+            case MusicDisc.Mellohi: return "record.mellohi";
+            case MusicDisc.Stal: return "record.stal";
+            case MusicDisc.Strad: return "record.strad";
+            case MusicDisc.Wait: return "record.wait";
+            case MusicDisc.Ward: return "record.ward";
+            case MusicDisc.Otherside: return "record.otherside";
+            case MusicDisc.Pigstep: return "record.pigstep";
+            default: throw `Unrecognized MusicDisc enum value: ${musicDisc}`;
         }
     }
 }
--- src/volume.ts
+++ src/volume.ts
@@ -1,12 +1,11 @@
 namespace music {
     const GAME_VOLUME_MIN = 0.0;
-    const GAME_VOLUME_MAX = 10.0;
+    const GAME_VOLUME_MAX = 1.0;
     const INPUT_VOLUME_MIN = 0;
-    const INPUT_VOLUME_MAX = 255;
+    const INPUT_VOLUME_MAX = 100;
 
-    // "game units" are what /playsound and /music commands expect for
-    // their volume parameters, a number from 0.0 to 10.0
-    export let volumeInGameUnits: number = 10.0;
+    // "game units" are what /playsound and /music commands expect for their volume parameters
+    export let volumeInGameUnits: number = GAME_VOLUME_MAX;
 
     /**
      * Returns the current output volume that will be used by sound and music
@@ -24,11 +23,11 @@ namespace music {
     /**
      * Set the default output volume to be used by sound and music commands.
      * This is separate from the Minecraft settings for volume!
-     * @param volume the volume 0...255
+     * @param volume the volume 0...100
      */
     //% blockId=sound_set_volume block="set volume %volume"
-    //% volume.min=0 volume.max=255
-    //% volume.defl=127
+    //% volume.min=0 volume.max=100
+    //% volume.defl=50
     //% help=sound/set-volume
     //% weight=70
     //% group="Volume"
